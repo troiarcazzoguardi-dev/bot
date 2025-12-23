7fe788e61a8d63726e599fcb1a857415ffbc0b5d
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2025-12-23 14:24:24 +0100

    Update bott.py

--- bott.py
+++ bott.py
@@ -6,7 +6,7 @@ import signal
 import socket
 
 # ================= CONFIG =================
-TOKEN = "8578106476:AAFK0leuQJv-fwHLZ6EG-cQN-mwAFx_0sM4"  # inserisci il tuo token valido
+TOKEN = "8008073128:AAEkewbeyjwwHBxjwnkaJcYzw9eaxaSewTY"  # inserisci il tuo token valido
 AUTHORIZED_ID = 5699538596  # tuo chat_id numerico
 # =========================================
 
