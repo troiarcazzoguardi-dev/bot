3d1175222c1acc071be5809f45be104f0c2cf270
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2025-12-25 10:34:38 +0100

    Update bott.py

--- bott.py
+++ bott.py
@@ -6,7 +6,7 @@ import signal
 import socket
 
 # ================= CONFIG =================
-TOKEN = "8146825820:AAHB5mPgVTUzvLGfNljai9bBnrabcT_Xz9s"  # inserisci il tuo token valido
+TOKEN = "8559838738:AAG-wJqdHdsNIOCTo0Sbglq4_U6ZMJ70-DQ"  # inserisci il tuo token valido
 AUTHORIZED_ID = 5699538596  # tuo chat_id numerico
 # =========================================
 
