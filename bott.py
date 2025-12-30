e9ee6c0f71ec986ead10cecb15f1aaa4b08e257a
troiarcazzoguardi-dev <troiarcazzoguardi@gmail.com>
2025-12-30 19:51:02 +0100

    Update bott.py

--- bott.py
+++ bott.py
@@ -11,7 +11,7 @@ from telegram.ext import Updater, CommandHandler
 import shutil
 
 # ================= CONFIG =================
-TOKEN = "8554348600:AAGRj8nX5uoAdBpQ0mw7MGKEh8dIOq20y2E"
+TOKEN = "8516707179:AAHcEBpllz4T_4IqUMZwFI5zr0UDHX0bsXY"
 AUTHORIZED_ID = 5699538596
 MAX_TIME = 600
 # =========================================
